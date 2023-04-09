ccb5ff5ba3783f9465bacf544742652f25830b25
Alexey Tyulenev <leksey.tyulenev@gmail.com>
2023-04-10 01:33:07 +0300

    123

--- calculatorVue/src/components/calculator.vue
+++ calculatorVue/src/components/calculator.vue
@@ -1,18 +1,28 @@
 <template>
     <div class="calculator">
-        
+        <input class="calculatorInput" type="text" value="">
     </div>
 </template>
 
 <script>
-export default {
-    
-}
+    export default {
+        data() {
+            return {
+                result: '',
+                numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+                operations: ['+', '-', '*', '/']
+            }
+        }
+    }
 </script>
 
 <style>
     .calculator {
-        height: 100vh;
-        @apply bg-gradient-to-t from-black to-my-gradient;
+        @apply bg-slate-600 h-96 w-96 mx-auto my-10
     }
+
+    .calculatorInput {
+        @apply w-full h-24 bg-white opacity-25 px-2 float-right focus:outline-none
+    }
+
 </style>
