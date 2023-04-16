20a4ddcc79d5a59cfdfc92240a26c25c46523357
Alexey Tyulenev <leksey.tyulenev@gmail.com>
2023-04-16 17:29:26 +0300

    123

--- calculatorVue/src/components/calculator.vue
+++ calculatorVue/src/components/calculator.vue
@@ -17,12 +17,5 @@
 </script>
 
 <style>
-    .calculator {
-        @apply bg-slate-600 h-96 w-96 mx-auto my-10
-    }
-
-    .calculatorInput {
-        @apply w-full h-24 bg-white opacity-25 px-2 float-right focus:outline-none
-    }
 
 </style>
---
+++ weather/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+  import btnComponent from './components/btnComponent.vue';
+</script>
+
+<template>
+  <btnComponent />
+</template>
+
+<style scoped>
+
+</style>
