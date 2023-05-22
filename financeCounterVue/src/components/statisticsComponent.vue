68fe0a63d259cf254e3d56f3d499dfacda6ff95f
alexey <81881832+lesha2701@users.noreply.github.com>
2023-05-22 18:22:00 +0300

    123

--- financeCounterVue/src/components/statisticsComponent.vue
+++ financeCounterVue/src/components/statisticsComponent.vue
@@ -2,19 +2,43 @@
     <div class="statistics">
         <div class="statistics-title">
             <h3 class="statistics-title-text">Информация о финансах</h3>
-            <a @click="updateInfo" href="#">Обновить статистику</a>
+            <a @click="updateInfo" href="#" class="statistics-title-update">Обновить статистику</a>
+            <p class="statistics-title-total">Общая сумма: {{ sumInc - sumExp }} руб.</p>
         </div>
         <div class="statistics-block">
             <div class="statistics-income statistics-block-item">
-                <p>Доходы</p>
-            </div>
-            <div class="statistics-expenses statistics-block-item">
-                <p>Расходы</p>
+                <p class="stat-title">Доходы</p>
                 <div class="allExp">
-                    <p>Общая сумма трат: {{ sumExp }}</p>
+                    <p class="stat-total">Общая сумма доходов: {{ sumInc }} руб.</p>
                 </div>
                 <div class="select-exp">
-                    <p>Максимальная и минимальная трата</p>
+                    <p>Информация по отдельной категории:</p>
+                    <div class="select-exp-option">
+                        <select v-model.lazy="categoryInc" class="form-stat" name="select" id="form-select">
+                            <option value="Зарплата">Зарплата</option>
+                            <option value="Стипендия">Стипендия</option>
+                            <option value="Родители">Родители</option>
+                            <option value="Подарок">Подарок</option>
+                            <option value="Прочее">Прочее</option>
+                        </select>
+                        <a @click="getInfoInc" href="#">Узнать</a>
+                    </div>
+                    <div v-if="canSeeInc" class="maxMinInfo">
+                        <ul class="infoItems">
+                            <li>Максимальный доход: {{ maxInc }} руб.</li>
+                            <li>Миниммальный доход: {{ minInc }} руб.</li>
+                            <li>Дата последнего дохода: {{ dateInc }}</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="statistics-expenses statistics-block-item">
+                <p class="stat-title">Расходы</p>
+                <div class="allExp">
+                    <p class="stat-total">Общая сумма трат: {{ sumExp }} руб.</p>
+                </div>
+                <div class="select-exp">
+                    <p>Информация по отдельной категории:</p>
                     <div class="select-exp-option">
                         <select v-model.lazy="category" class="form-stat" name="select" id="form-select">
                             <option value="Еда">Еда</option>
@@ -27,8 +51,11 @@
                         <a @click="getInfo" href="#">Узнать</a>
                     </div>
                     <div v-if="canSeeInfo" class="maxMinInfo">
-                        <p>Максимальная трата на еду: {{ max }} руб.</p>
-                        <p>Миниммальная трата на еду: {{ min }} руб.</p>
+                        <ul class="infoItems">
+                            <li>Максимальная трата: {{ max }} руб.</li>
+                            <li>Миниммальная трата: {{ min }} руб.</li>
+                            <li>Дата последней траты: {{ date }}</li>
+                        </ul>
                     </div>
                 </div>
             </div>
@@ -43,20 +70,34 @@ export default {
         return {
             category: '',
             canSeeInfo: false,
+            categoryInc: '',
+            canSeeInc: false,
             max: '',
             min: '',
+            maxInc: '',
+            minInc: '',
+            date: '',
+            dateInc: '',
             sumExp: 0,
+            sumInc: 0,
             expenses: []
         }
     },
     mounted() {
         axios
-            .get(`http://localhost:3000/expenses`)
-            .then(response => {
-                response.data.forEach(element => {
-                    this.sumExp += element.price
-                });
+        .get(`http://localhost:3000/expenses`)
+        .then(response => {
+            response.data.forEach(element => {
+                this.sumExp += element.price
             });
+        });
+        axios
+        .get(`http://localhost:3000/income`)
+        .then(response => {
+            response.data.forEach(element => {
+                this.sumInc += element.price
+            });
+        });
     },
     methods: {
         getInfo() {
@@ -70,10 +111,34 @@ export default {
             .get(`http://localhost:3000/expenses?category=${this.category}&_sort=price&_order=asc`)
             .then((response) => {
                 this.min= response.data[0].price
-            })
+            });
+            axios
+            .get(`http://localhost:3000/expenses?_sort=date&_order=desc`)
+            .then((response) => {
+                this.date = response.data[0].date
+            });
+        },
+        getInfoInc() {
+            this.canSeeInc = true;
+            axios
+            .get(`http://localhost:3000/income?category=${this.categoryInc}&_sort=price&_order=desc`)
+            .then(response => {
+                this.maxInc = response.data[0].price
+            });
+            axios
+            .get(`http://localhost:3000/income?category=${this.categoryInc}&_sort=price&_order=asc`)
+            .then(response => {
+                this.minInc = response.data[0].price
+            });
+            axios
+            .get(`http://localhost:3000/income?_sort=date&_order=desc`)
+            .then((response) => {
+                this.dateInc = response.data[0].date
+            });
         },
         updateInfo() {
             this.sumExp = 0
+            this.sumInc = 0
             axios
             .get(`http://localhost:3000/expenses`)
             .then(response => {
@@ -81,6 +146,13 @@ export default {
                     this.sumExp += element.price
                 });
             });
+            axios
+            .get(`http://localhost:3000/income`)
+            .then(response => {
+                response.data.forEach(element => {
+                    this.sumInc += element.price
+                });
+            })
         }
     }
 }
@@ -96,7 +168,7 @@ export default {
 
     .statistics-title {
         text-align: center;
-        margin: 10px 0;
+        margin: 10px 0 5px;
     }
 
     .statistics-title-text {
@@ -119,5 +191,48 @@ export default {
         color: #ccc;
         transition: all .3s;
         width: 150px;
+    }
+
+    .statistics-block-item {
+        box-sizing: border-box;
+        padding: 10px 5px;
+    }
+
+    .stat-title {
+        font-size: 18px;
+        margin-bottom: 10px;
+        text-decoration: underline;
+    }
+
+    .stat-total {
+        margin-bottom: 10px;
+    }
+
+    .select-exp {
+        display: flex;
+        flex-direction: column;
+        gap: 5px;    
+    }
+
+    .select-exp-option {
+        display: flex;
+        align-items: center;
+        gap: 20px;
+    }
+
+    .statistics-title-total {
+        font-size: 20px;
+        margin-top: 10px;
+    }
+
+    .infoItems {
+        padding-left: 15px;
+        display: flex;
+        flex-direction: column;
+        gap: 5px;
+    }
+
+    .infoItems li {
+        padding: 0;
     }
 </style>
